d513b8adcf8ae763890071cee32f3f2aa51ad8a6
Bruno <brunomonteiro.sud@gmail.com>
2019-07-02 00:06:53 +1200

    Updated JS to ES6 arrow function and applied Clean Code principle: Separation of Concern by moving the product mapping function outside the AJAX call

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,12 @@
 // Waits for document to load
-$(document).ready(function(){
+$(document).ready(() => {
     $.ajax({
         url: "https://api.jsonbin.io/b/5cae9a54fb42337645ebcad3",
-        success: function(data) {
-            // Clear product display block
-            $("#productsDisplay").html("");
-
-            // Process JSON data to format it in correct HTML template
-            var productCards = data.map(function(product){
-                return(`
-                    <div class="col-12 col-md-6 col-lg-3 p-0">
-                        <div class="product-card">
-                            <figure>
-                                <img class="img-fluid" src="./images/products/${product.productImage}">
-                            </figure>
-                            <div class="tag">
-                                ${product.isSale ? '<span class="sale">Sale</span>' : ''}
-                                ${product.isExclusive ? '<span class="exclusive">Exclusive</span>' : ''}
-                            </div>
-                            <div class="product-info">
-                                <span class="name">${product.productName}</span>
-                                <span class="price">${product.price}</span>
-                            </div>
-                        </div>
-                    </div>
-                `);
-            });
-
-            // Append formatted HTML
-            $("#productsDisplay").append(productCards);
-            
+        success: (data) => {
+            appendProducts(data);
+            // createFilter(data);
         },
-        error: function(error) {
+        error: (error) => {
             // Displays error message if data was not loaded
             $("#productsDisplay").html(`
                 <div class="alert alert-danger w-100" role="alert">
@@ -45,3 +20,35 @@ $(document).ready(function(){
         }
     });
 });
+
+const appendProducts = (products) => {
+    // Clear product display block
+    $("#productsDisplay").html("");
+
+    // Process JSON data to format it in correct HTML template
+    var productCards = products.map(function(product){
+        return(`
+            <div class="col-12 col-md-6 col-lg-3 p-0">
+                <div class="product-card">
+                    <figure>
+                        <img class="img-fluid" src="./images/products/${product.productImage}">
+                    </figure>
+                    <div class="tag">
+                        ${product.isSale ? '<span class="sale">Sale</span>' : ''}
+                        ${product.isExclusive ? '<span class="exclusive">Exclusive</span>' : ''}
+                    </div>
+                    <div class="product-info">
+                        <span class="name">${product.productName}</span>
+                        <span class="price">${product.price}</span>
+                    </div>
+                </div>
+            </div>
+        `);
+    });
+
+    // Append formatted HTML
+    $("#productsDisplay").append(productCards);
+}
+
+const createFilter = (products) => {
+}
