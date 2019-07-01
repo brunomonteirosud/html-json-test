2e15328c3d91e52e8bf1af028016dfee0f1db0d1
Bruno <brunomonteiro.sud@gmail.com>
2019-07-01 22:33:45 +1200

    Downloaded products images and connected to API

---
+++ scripts/constants.js
@@ -0,0 +1,1 @@
+export const JSON_URL = "https://api.jsonbin.io/b/5cae9a54fb42337645ebcad3";
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $.ajax({
+        url: "https://api.jsonbin.io/b/5cae9a54fb42337645ebcad3",
+        success: function(data) {
+            console.log("data", data);
+        },
+        error: function(error) {
+            console.log("error", error.responseJSON.message);
+        }
+    });
+});
