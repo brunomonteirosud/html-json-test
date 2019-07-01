ec760d71a87d716ae717979bfecfad47f0f66316
Bruno <brunomonteiro.sud@gmail.com>
2019-07-01 23:35:35 +1200

    Added skeleton loading effect

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,18 @@ $(document).ready(function(){
         url: "https://api.jsonbin.io/b/5cae9a54fb42337645ebcad3",
         success: function(data) {
             console.log("data", data);
+            // <div class="col-12 col-sm-6 col-md-3 p-0">
+            //     <div class="product-card">
+            //         <figure>
+            //             <img class="img-fluid" src="./images/products/product-1.jpg">
+            //         </figure>
+            //         <span class="tag exclusive">Exclusive</span>
+            //         <div class="product-info">
+            //             <span class="name">Product name</span>
+            //             <span class="price">$15.00</span>
+            //         </div>
+            //     </div>
+            // </div>
         },
         error: function(error) {
             console.log("error", error.responseJSON.message);
